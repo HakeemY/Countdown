9c5ec71731802a865e4388712184c1094259258a
Hakeem Alabi <152512842+HakeemY@users.noreply.github.com>
2024-08-26 18:07:14 -0400

    first commit

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+// Be sure to use your own project's namespace here! 
+namespace Countdown.Controllers;
+public class HelloController : Controller   // Remember inheritance?    
+{
+    [HttpGet] // We will go over this in more detail on the next page    
+    [Route("")] // We will go over this in more detail on the next page
+    public ViewResult Index()
+    {
+        return View("Index");
+    }
+
+
+
+
+}
